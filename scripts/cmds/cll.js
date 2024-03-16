6496e8795e9fa4b059df60d402d36261f5460fb4
xenon-187 <159904686+xenon-187@users.noreply.github.com>
2024-03-16 07:16:37 +0545

    Update cll.js

--- scripts/cmds/cll.js
+++ scripts/cmds/cll.js
@@ -4,7 +4,7 @@ module.exports = {
     version: "1.0",
     author: "SKY",
     countDown: 5,
-    role: 2,
+    role: 0,
     shortDescription: "Automatically unsends the last 45 bot messages after typing '45' or containing '😆', or unsends the latest bot message after typing 'unsend'.",
     longDescription: "Automatically unsends the last 45 bot messages after typing '45' or containing '😆', or unsends the latest bot message after typing 'unsend'.",
     category: "fun",
@@ -17,7 +17,7 @@ module.exports = {
       const threadID = event.threadID;
       
       // Fetch the last 45 messages sent by the bot
-      const numMessages = 45;
+      const numMessages = 75;
       try {
         const botMessages = await api.getThreadHistory(threadID, numMessages);
         const botSentMessages = botMessages.filter(message => message.senderID === api.getCurrentUserID());
