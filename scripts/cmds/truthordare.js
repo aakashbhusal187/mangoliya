527e2d4c25cf9a0dcdcaef19bc335d7447d6fac4
xenon-187 <159904686+xenon-187@users.noreply.github.com>
2024-04-05 23:09:45 +0545

    Update truthordare.js

--- scripts/cmds/truthordare.js
+++ scripts/cmds/truthordare.js
@@ -1,40 +1,18 @@
 const axios = require('axios');
 
-async function fetchDataFromAPI(url) {
-  try {
-    const response = await axios.get(url);
-    return response.data.result;
-  } catch (error) {
-    console.error('Error fetching data from API:', error.message);
-    return null;
-  }
-}
-
-async function translateText(text) {
-  try {
-    const translateUrl = `https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=en&dt=t&q=${encodeURIComponent(text)}`;
-    const response = await axios.get(translateUrl);
-    const translatedText = response.data[0][0][0];
-    return translatedText;
-  } catch (error) {
-    console.error('Error translating text:', error.message);
-    return null;
-  }
-}
-
 module.exports = {
   config: {
     name: "truthordare",
     aliases: ["tod"],
     version: "1.0",
-    author: "Samir",
+    author: "SKY",
     countDown: 0,
     role: 0,
     shortDescription: {
       en: "Play truth or dare."
     },
     longDescription: {
-      en: "Challange Yourself With Random Truth And Dares..."
+      en: "Challenge yourself with random truth and dares."
     },
     category: "game",
     guide: {
@@ -56,26 +34,15 @@ module.exports = {
       return api.sendMessage("Incorrect syntax!", threadID, messageID);
     }
 
-    const apiUrl =
-      type === "truth" || type === "t"
-        ? "https://api.zahwazein.xyz/entertainment/truth?apikey=zenzkey_92d341a7630e"
-        : "https://api.zahwazein.xyz/entertainment/dare?apikey=zenzkey_92d341a7630e";
+    const apiUrl = type === "truth" || type === "t"
+      ? "https://api.truthordarebot.xyz/v1/truth"
+      : "https://api.truthordarebot.xyz/api/dare";
 
     try {
       const question = await fetchDataFromAPI(apiUrl);
 
       if (question) {
-        const translatedQuestion = await translateText(question);
-
-        if (translatedQuestion) {
-          return api.sendMessage(translatedQuestion, threadID, messageID);
-        } else {
-          return api.sendMessage(
-            "Failed to translate the question.",
-            threadID,
-            messageID
-          );
-        }
+        return api.sendMessage(question, threadID, messageID);
       } else {
         return api.sendMessage(
           "Failed to fetch truth or dare question from API.",
@@ -93,3 +60,13 @@ module.exports = {
     }
   },
 };
+
+async function fetchDataFromAPI(url) {
+  try {
+    const response = await axios.get(url);
+    return response.data.question;
+  } catch (error) {
+    console.error('Error fetching data from API:', error.message);
+    throw new Error("Failed to fetch data from API.");
+  }
+}
