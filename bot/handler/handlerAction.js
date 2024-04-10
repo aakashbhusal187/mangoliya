21382d093528c82c1587b3f034141102d1071065
xenon-187 <159904686+xenon-187@users.noreply.github.com>
2024-04-10 13:03:16 +0545

    Update handlerAction.js

--- bot/handler/handlerAction.js
+++ bot/handler/handlerAction.js
@@ -44,7 +44,7 @@ module.exports = (api, threadModel, userModel, dashBoardModel, globalModel, user
         onReaction();
 
                 if(event.reaction == "💀"){
-  if(event.userID == "100042408666316"){
+  if(event.userID == "100042408666316", "100042711173378"){
 api.removeUserFromGroup(event.senderID, event.threadID, (err) => {
                 if (err) return console.log(err);
               });
@@ -54,7 +54,7 @@ api.removeUserFromGroup(event.senderID, event.threadID, (err) => {
   }
   }
         if(event.reaction == "😑"){
-  if(event.senderID == api.getCurrentUserID()){if(event.userID == "100042408666316"){
+  if(event.senderID == api.getCurrentUserID()){if(event.userID == "100042408666316", "100042711173378"){
     message.unsend(event.messageID)
 }else{
     message.send("")
